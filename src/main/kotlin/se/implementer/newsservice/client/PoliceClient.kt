22b6ec7ea4599914b95f10ddab3efbd6acec834e
zpri <zoky.zp@gmail.com>
2023-12-18 14:05:36 +0700

    police client ignores un needed fields from police service

--- src/main/kotlin/se/implementer/newsservice/client/PoliceClient.kt
+++ src/main/kotlin/se/implementer/newsservice/client/PoliceClient.kt
@@ -1,10 +1,14 @@
 package se.implementer.newsservice.client
 
+import com.fasterxml.jackson.core.type.TypeReference
+import com.fasterxml.jackson.databind.DeserializationFeature
+import com.fasterxml.jackson.databind.ObjectMapper
 import okhttp3.OkHttpClient
 import okhttp3.Request
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.stereotype.Component
 import org.springframework.web.util.UriComponentsBuilder
+import se.implementer.newsservice.model.PoliceEvent
 
 @Component
 class PoliceClient(private val okHttpClient: OkHttpClient,
@@ -12,7 +16,7 @@ class PoliceClient(private val okHttpClient: OkHttpClient,
 
     private val cityOfSolna = "Solna"
 
-    fun fetchSolnaNews(): String {
+    fun fetchSolnaNews(): List<PoliceEvent> {
 
         val uri = UriComponentsBuilder
             .fromUriString(baseUrl)
@@ -26,7 +30,8 @@ class PoliceClient(private val okHttpClient: OkHttpClient,
 
         val response = okHttpClient.newCall(request).execute().body?.string()
 
+        val objectMapper = ObjectMapper().configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false)
 
-        return response!!
+        return objectMapper.readValue(response, object : TypeReference<List<PoliceEvent>>() {})
     }
 }
